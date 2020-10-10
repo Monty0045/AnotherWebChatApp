7f7f77827b2bb4cc633031654c4f4d65a5ee5e18
Wyatt Turner <wyatturner.45@gmail.com>
2020-10-10 15:59:02 -0700

    removed placeholder

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,6 @@ import Chat from './components/Chat';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello team! This all HIIII is the second test.
-        </a>
-      </header>
       <Chat />
     </div>
   );
