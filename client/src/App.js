c5eb26177900d03273fff7bbed0de11074a26e99
mattzamora21 <68615568+mattzamora21@users.noreply.github.com>
2020-10-10 21:51:05 -0700

    changed localhost location

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Chat from './components/Chat';
 
 
 const io = require("socket.io-client");
-let socket = io.connect("http://localhost:3000"); //connects to server with specific url using port 3001
+let socket = io.connect("http://localhost:3001"); //connects to server with specific url using port 3001
 socket.on("joined", (data) =>   //if socket is online with message joined (everything went correctly)
 {  
   socket.send("14");
